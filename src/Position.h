27443b9b7cb375927191bda623f98c0729dddaa3
Gerry Agbobada <gagbobada+git@gmail.com>
2018-03-02 00:50:18 +0100

    Add comments

--- src/Position.h
+++ src/Position.h
@@ -2,13 +2,20 @@
 #define _POSITION_H_
 #include <cmath>
 
+// Class that represents a position on the map
 struct Position {
+  // Row index (given first in (r,c) tuples in Google's syntax)
   int row;
+  // Column index (given second in (r,c) tuples in Google's syntax)
   int col;
 
-  Position(int r, int c) : row(r), col(c) {;}
+  // Overloaded constructor
+  Position(int r, int c) : row(r), col(c) { ; }
+  // Default constructor
+  Position() : row(0), col(0) { ; }
 };
 
-int distance(const Position& a, const Position& b);
+// Compute the Manhattan distance between 2 positions
+int distance(const Position &a, const Position &b);
 
 #endif //_POSITION_H_
